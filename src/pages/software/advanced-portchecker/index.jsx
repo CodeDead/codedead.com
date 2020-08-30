99f50f1adbc386ab0b39511a9f66d197ac8e9ac8
CodeDead <admin@codedead.com>
2020-08-31 01:17:11 +0200

    * Added page for advanced portchecker

--- src/pages/software/advanced-portchecker/index.jsx
+++ src/pages/software/advanced-portchecker/index.jsx
@@ -1,8 +1,273 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
+import { graphql, useStaticQuery } from 'gatsby';
+import PageHeader from '../../../components/PageHeader';
+import { Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Img from 'gatsby-image';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 
-const AdvancedPortCheckerPage = () => (
-  <Layout />
-);
+const AdvancedPortCheckerPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    main: file(relativePath: { eq: "Advanced PortChecker/ap.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    settings: file(relativePath: { eq: "Advanced PortChecker/ap_general_settings.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    about: file(relativePath: { eq: "Advanced PortChecker/ap_about.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }`);
+
+  return (
+    <Layout>
+      <PageHeader title="Advanced PortChecker" subTitle="Check TCP/UDP ports"/>
+      <Container maxWidth="md" style={{ marginTop: 10 }}>
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              General
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Port scanning
+                </Typography>
+                <Typography paragraph>
+                  Scanning TCP and UDP ports has never been this easy. Watch how Advanced
+                  PortChecker updates you with real-time information while it’s scanning!
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.main.childImageSharp.fixed}/>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.settings.childImageSharp.fixed}/>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Preferences
+                </Typography>
+                <Typography paragraph>
+                  Need a specific time-out set for port scanning? Want a different theme or would
+                  you like to disable automatic updates? It’s all possible, thanks to our intuitive
+                  and easy to use GUI.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Exporting
+                </Typography>
+                <Typography paragraph>
+                  Advanced PortChecker can export any and all data that it finds for you in a couple
+                  of different formats including: HTML, CSV and of course plain text.
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.about.childImageSharp.fixed}/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              Download
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={6} lg={6}>
+            <Typography variant="h6" align="center">
+              Android
+            </Typography>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{ width: '100%' }}
+              href="https://play.google.com/store/apps/details?id=com.codedead.advancedportchecker"
+            >
+              Play Store
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6} lg={6}>
+            <Typography variant="h6" align="center">
+              Windows
+            </Typography>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{ width: '100%' }}
+              href="https://codedead.com/Software/Advanced%20PortChecker/AP_SETUP.exe"
+            >
+              Installer
+            </Button>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{
+                width: '100%',
+                marginTop: 10
+              }}
+              href="https://codedead.com/Software/Advanced%20PortChecker/AP_portable.zip"
+            >
+              Portable
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              What others say
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" color="textPrimary" gutterBottom>
+                  Handy tool providing you with results in real time
+                </Typography>
+                <Typography gutterBottom>
+                  <i>
+                    &quot;On an ending note, Advanced PortChecker is an easy-to-use program helping
+                    you effortlessly find open ports you can connect to. It can spot both TCP and
+                    UDP ports, letting you compile them in lists you can subsequently use, but its
+                    core advantage is that it helps you see the potential of your network in real
+                    time.&quot;
+                  </i>
+                </Typography>
+                <Typography>
+                  <b>Anca Roman</b>
+                </Typography>
+                <Typography>
+                  Softpedia editor
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  href="https://www.softpedia.com/get/Network-Tools/IP-Tools/Advanced-PortChecker.shtml"
+                  target="_blank"
+                >
+                  Source
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              Extra
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="/software/advanced-portchecker/requirements"
+              style={{ width: '100%' }}
+            >
+              Requirements
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://codedead.com/Software/Advanced%20PortChecker/help.pdf"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              Help documentation
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://codedead.com/Software/Advanced%20PortChecker/gpl.pdf"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              License
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://github.com/CodeDead/Advanced-PortChecker"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              Source code
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
 
 export default AdvancedPortCheckerPage;
