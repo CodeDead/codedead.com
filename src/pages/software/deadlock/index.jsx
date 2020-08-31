3e1a8ae1d17df9863f209e88f8ec1882cec10f90
CodeDead <admin@codedead.com>
2020-08-31 02:46:22 +0200

    * Added page for deadpix

--- src/pages/software/deadlock/index.jsx
+++ src/pages/software/deadlock/index.jsx
@@ -1,8 +1,249 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
+import { graphql, useStaticQuery } from 'gatsby';
+import PageHeader from '../../../components/PageHeader';
+import { Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Img from 'gatsby-image';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 
-const DeadLockPage = () => (
-  <Layout />
-);
+const DeadLockPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    main: file(relativePath: { eq: "DeadLock/deadlock.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    settings: file(relativePath: { eq: "DeadLock/settings.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    about: file(relativePath: { eq: "DeadLock/about.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }`);
+
+  return (
+    <Layout>
+      <PageHeader title="DeadLock" subTitle="Unlock files and folders"/>
+      <Container maxWidth="md" style={{ marginTop: 10 }}>
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              General
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Unlocking
+                </Typography>
+                <Typography paragraph>
+                  Unlock files and folders, take full ownership over them and remove, move or copy
+                  them, free of charge!
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.main.childImageSharp.fixed}/>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.settings.childImageSharp.fixed}/>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Preferences
+                </Typography>
+                <Typography paragraph>
+                  Want a different theme or would you like to disable automatic updates? Want to
+                  enable Windows Explorer integration? It’s all possible thanks to our many
+                  settings!
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  CLI
+                </Typography>
+                <Typography paragraph>
+                  You don’t have to open the GUI to unlock files or folders. You can jump straight
+                  into PowerShell or CMD to pass along arguments to DeadLock!
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.about.childImageSharp.fixed}/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              Download
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{ width: '100%' }}
+              href="https://codedead.com/Software/DeadLock/dl_setup.exe"
+            >
+              Installer
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              What others say
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" color="textPrimary" gutterBottom>
+                  Scans your items on VirusTotal
+                </Typography>
+                <Typography gutterBottom>
+                  <i>
+                    &quot;
+                    Aside from the functions mentioned above, this application also allows you to
+                    perform scans on your files with the online VirusTotal service.
+                    <br/><br/>
+                    To sum it up, DeadLock is a lightweight application that enables you to unlock
+                    files that are restricted by certain processes on your computer in a convenient,
+                    effortless manner. It comes with a minimalistic, yet efficient user interface
+                    and despite its high overall accessibility, it also features extensive help
+                    documentation.
+                    &quot;
+                  </i>
+                </Typography>
+                <Typography>
+                  <b>Vlad Constantinescu</b>
+                </Typography>
+                <Typography>
+                  Softpedia editor
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  href="https://www.softpedia.com/get/System/File-Management/DeadLock-CodeDead.shtml"
+                  target="_blank"
+                >
+                  Source
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              Extra
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider/>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="/software/deadlock/requirements"
+              style={{ width: '100%' }}
+            >
+              Requirements
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://codedead.com/Software/DeadLock/help.pdf"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              Help documentation
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://codedead.com/Software/DeadLock/gpl.pdf"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              License
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://github.com/CodeDead/DeadLock"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              Source code
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
 
 export default DeadLockPage;
--- src/pages/software/deadpix/index.jsx
+++ src/pages/software/deadpix/index.jsx
@@ -1,8 +1,261 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Img from 'gatsby-image';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import PageHeader from '../../../components/PageHeader';
 import Layout from '../../../components/Layout';
 
-const DeadPixPage = () => (
-  <Layout />
-);
+const DeadPixPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    main: file(relativePath: { eq: "DeadPix/deadpix.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    settings: file(relativePath: { eq: "DeadPix/settings.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    advancedpassgensettingsadvanced: file(relativePath: { eq: "Advanced PassGen/ap_advanced_settings.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }`);
+
+  return (
+    <Layout>
+      <PageHeader title="DeadPix" subTitle="Fix broken pixels" />
+      <Container maxWidth="md" style={{ marginTop: 10 }}>
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              General
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider />
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Fix your display
+                </Typography>
+                <Typography paragraph>
+                  Fix dead or stuck pixels on your screen with just a couple of mouse clicks, free
+                  of charge!
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.main.childImageSharp.fixed} />
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={6}>
+                <Img fixed={data.settings.childImageSharp.fixed} />
+              </Grid>
+              <Grid item xs={12} md={6} lg={6}>
+                <Typography variant="h6">
+                  Method of action
+                </Typography>
+                <Typography paragraph>
+                  DeadPix can fix stuck or dead pixels by rapidly cycling the color value of the
+                  pixel. Please note that this tool does not provide a 100% success rate.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h6">
+              Installation
+            </Typography>
+            <Typography paragraph>
+              DeadPix can be installed using the installer or if you don’t want to install it,
+              you can always use the portable version!
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              Download
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={6} lg={6}>
+            <Typography variant="h6" align="center">
+              Android
+            </Typography>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{ width: '100%' }}
+              href="https://play.google.com/store/apps/details?id=com.codedead.deadpix"
+            >
+              Play Store
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6} lg={6}>
+            <Typography variant="h6" align="center">
+              Windows
+            </Typography>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{ width: '100%' }}
+              href="https://codedead.com/Software/DeadPix/DP_setup.exe"
+            >
+              Installer
+            </Button>
+            <Button
+              size="large"
+              color="primary"
+              variant="outlined"
+              style={{ width: '100%', marginTop: 10 }}
+              href="https://codedead.com/Software/DeadPix/DP_Port.zip"
+            >
+              Portable
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              What others say
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" color="textPrimary" gutterBottom>
+                  A handy dead pixel finder
+                </Typography>
+                <Typography gutterBottom>
+                  <i>
+                    &quot;Overall, DeadPix proves to be a practical program that you can resort to
+                    for locating and trying to fix stuck pixels on your screen by flashing random
+                    colors for a lengthier period of time; however, you should not expect
+                    miracles.&quot;
+                  </i>
+                </Typography>
+                <Typography>
+                  <b>Marina Dan</b>
+                </Typography>
+                <Typography>
+                  Softpedia editor
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  href="https://www.softpedia.com/get/Others/Miscellaneous/DeadPix.shtml"
+                  target="_blank"
+                >
+                  Source
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography variant="h5" color="textPrimary">
+              Extra
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Divider />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{ marginTop: 10 }}>
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="/software/deadpix/requirements"
+              style={{ width: '100%' }}
+            >
+              Requirements
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://codedead.com/Software/DeadPix/help.pdf"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              Help documentation
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} md={4} lg={4}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://codedead.com/Software/DeadPix/gpl.pdf"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              License
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Button
+              color="primary"
+              variant="outlined"
+              href="https://github.com/CodeDead/DeadPix"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              Source code
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
 
 export default DeadPixPage;
