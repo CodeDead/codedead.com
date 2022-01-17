90aa847d3d594947a55d08675ee9a446b7c1a959
CodeDead <admin@codedead.com>
2022-01-17 18:49:04 +0100

    Removed more reddit references

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -21,7 +21,6 @@ const Layout = ({ children }) => {
           title,
           github,
           twitter,
-          reddit,
           facebook
         }
       }
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -25,7 +25,6 @@ const AboutPage = () => {
       site {
         siteMetadata {
           github,
-          reddit,
         }
       }
      }`);
