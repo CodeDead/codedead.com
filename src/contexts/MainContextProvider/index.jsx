8ee50cefd70f1365817bbf52d9336d04d1ff5746
CodeDead <admin@codedead.com>
2023-03-22 21:50:03 +0100

    Added lint script

--- src/contexts/MainContextProvider/index.jsx
+++ src/contexts/MainContextProvider/index.jsx
@@ -23,6 +23,7 @@ export const MainContext = createContext(initState);
 const MainContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(MainReducer, initState);
   return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
     <MainContext.Provider value={[state, dispatch]}>
       {children}
     </MainContext.Provider>
