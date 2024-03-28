a3efac4d485c56e3be74c0a75c97ee22a3b92bea
CodeDead <admin@codedead.com>
2024-03-28 03:00:28 +0100

    feat: added Opal release notes, dependency upgrades

---
+++ src/markdown/blog/2024/03/28/opal-1.3.1.md
@@ -0,0 +1,33 @@
+---
+path: "/blog/2024/03/28/opal-1.3.1"
+title: "Opal 1.3.1 - Release"
+author: "CodeDead"
+date: "2024-03-28"
+abstract: "Opal v1.3.1 has just been released. We've mainly focused on bringing you dependency upgrades and security upgrades for the included Java runtime. Some minor UI..." 
+categories: "Java, News, Opal"
+---
+## Information
+
+![Opal](https://i.imgur.com/UAxfclq.png)
+
+Opal v1.3.1 has just been released. We've mainly focused on bringing you dependency upgrades and security upgrades for the included Java runtime. Some minor UI changes were made to make the user-experience more enjoyable.
+In addition, this release should now also work on more modern Linux distributions like the upcoming Fedora 40 release.
+
+## What's Changed
+
+* feat: Gradle upgrade, dependency upgrades by @CodeDead in https://github.com/CodeDead/opal/pull/116
+* Feature/upgrades by @CodeDead in https://github.com/CodeDead/opal/pull/117
+* feat: refactoring by @CodeDead in https://github.com/CodeDead/opal/pull/118
+* feat: updated help docs, Gradle upgrade, dependency upgrades by @CodeDead in https://github.com/CodeDead/opal/pull/119
+* Release/v1.3.1 by @CodeDead in https://github.com/CodeDead/opal/pull/120
+
+**Full Changelog**: https://github.com/CodeDead/opal/compare/v1.3.0...v1.3.1
+
+## Download
+
+You can download the latest version of Opal by clicking on the following link:
+[Opal](https://codedead.com/software/opal)
+
+## Other
+
+Feel free to [contact us](/contact) if you have any questions or if you need help.
--- src/pages/software/opal/index.jsx
+++ src/pages/software/opal/index.jsx
@@ -114,7 +114,7 @@ const OpalPage = () => {
                   <Grid item xs={12} md={6} lg={6}>
                     <GatsbyImage
                       image={data.opalTimer.childImageSharp.gatsbyImageData}
-                      alt="Opal Timer"
+                      alt="Opal timer"
                     />
                   </Grid>
                   <Grid item xs={12} md={6} lg={6}>
@@ -178,7 +178,7 @@ const OpalPage = () => {
               variant="contained"
               color="primary"
               onClick={() => {
-                openLink('https://codedead.com/Software/Opal/Opal-1.3.0.msi');
+                openLink('https://codedead.com/Software/Opal/Opal-1.3.1.msi');
                 setDonateOpen(true);
               }}
             >
@@ -190,7 +190,7 @@ const OpalPage = () => {
               variant="contained"
               color="primary"
               onClick={() => {
-                openLink('https://codedead.com/Software/Opal/Opal-win-portable-1.3.0.zip');
+                openLink('https://codedead.com/Software/Opal/Opal-win-portable-1.3.1.zip');
                 setDonateOpen(true);
               }}
             >
@@ -206,7 +206,7 @@ const OpalPage = () => {
               variant="contained"
               color="primary"
               onClick={() => {
-                openLink('https://codedead.com/Software/Opal/Opal-1.3.0.dmg');
+                openLink('https://codedead.com/Software/Opal/Opal-1.3.1.dmg');
                 setDonateOpen(true);
               }}
             >
@@ -222,7 +222,7 @@ const OpalPage = () => {
               variant="contained"
               color="primary"
               onClick={() => {
-                openLink('https://codedead.com/Software/Opal/Opal-x86_64-1.3.0.AppImage');
+                openLink('https://codedead.com/Software/Opal/Opal-x86_64-1.3.1.AppImage');
                 setDonateOpen(true);
               }}
             >
