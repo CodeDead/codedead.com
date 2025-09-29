24c0cdebd65d6e9745902c374a8ad09dbf478302
CodeDead <admin@codedead.com>
2025-09-30 01:17:40 +0200

    feat: dependency upgrades

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -18,7 +18,7 @@ const App = ({ Component, pageProps }) => {
         <AppShell
           header={{ height: 60 }}
           navbar={{ width: 330, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-          footer={{ height: 100 }}
+          footer={{ height: 95 }}
           padding="md"
         >
           <AppShell.Header>
